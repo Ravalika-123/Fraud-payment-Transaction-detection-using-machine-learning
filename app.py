3a2b72b979223d3eb7ef948b3f1c2715417637ca
Ravalika-123 <130036235+Ravalika-123@users.noreply.github.com>
2024-04-24 10:59:30 +0530

    app.py

--- app.py
+++ app.py
@@ -1,52 +1,23 @@
-# app.py
-
-from flask import Flask, render_template, request
+import streamlit as st
 import numpy as np
-import pickle
 
-app = Flask(__name__)
+def main():
+    st.title("Fraud Payment Transaction Detection")
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+    transaction_type = st.selectbox("Transaction Type", ["Credit", "Debit"])
+    amount = st.number_input("Amount")
+    new_balance_sender = st.number_input("New Balance Sender")
+    old_balance_sender = st.number_input("Old Balance Sender")
+    new_balance_receiver = st.number_input("New Balance Receiver")
+    old_balance_receiver = st.number_input("Old Balance Receiver")
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-
-        # Extract input values from the form
-        transaction_type = request.form['transaction_type']
-        amount = float(request.form['amount'])
-        new_balance_sender = float(request.form['new_balance_sender'])
-        old_balance_sender = float(request.form['old_balance_sender'])
-        new_balance_receiver = float(request.form['new_balance_receiver'])
-        old_balance_receiver = float(request.form['old_balance_receiver'])
-
-        # Create input array for prediction
-        input_data = np.array([[transaction_type, amount, new_balance_sender, old_balance_sender, new_balance_receiver, old_balance_receiver]])
-
-        if (transaction_type=='4' and old_balance_receiver==0 and new_balance_receiver==0 and new_balance_sender==0) or (transaction_type=='3' and new_balance_receiver==0 and new_balance_sender==0):
+    if st.button("Predict"):
+        if (transaction_type == "Debit" and old_balance_receiver == 0 and new_balance_receiver == 0 and new_balance_sender == 0) or (transaction_type == "Credit" and new_balance_receiver == 0 and new_balance_sender == 0):
             output = "Alert!!! Fraud Transaction!"
         else:
-            output = "Not Fraud Transaction!"
+            output = "Not a Fraud Transaction!"
 
-
-
-
-
-
-        #
-        # # Determine output
-        # if prediction[0] == 1:
-        #     output = "Fraud Transaction"
-        # elsoutput = "Fraud Transaction"e:
-        #     output = "Not a Fraud Transaction"
-
-        return render_template('result.html', prediction=output)
-
-    except Exception as e:
-        print("Error:", e)
-
+        st.write(output)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
